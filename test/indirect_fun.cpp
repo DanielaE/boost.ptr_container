dac685483bd7ff0e3f28cc006d2f8d89a2047f4c
Daniela Engert <dani@ngrt.de>
2015-02-21 17:48:01 +0100

    Fix compiler warnings about unused formal parameters and narrowing conversions.
    Signed-off-by: Daniela Engert <dani@ngrt.de>

--- test/indirect_fun.cpp
+++ test/indirect_fun.cpp
@@ -70,7 +70,7 @@ void test_fun()
 #include <boost/test/unit_test.hpp>
 using boost::unit_test::test_suite;
 
-test_suite* init_unit_test_suite( int argc, char* argv[] )
+test_suite* init_unit_test_suite( int, char*[] )
 {
     test_suite* test = BOOST_TEST_SUITE( "Pointer Container Test Suite" );
 
